e14c9f2682c75984f268d1ac9d28e92a762850b4
Tom Needham <06needhamt@gmail.com>
2021-08-07 14:27:11 +0100

    Fix Plugin Loading :^)

--- src/main/gen/org/serenityos/gml/plugin/parser/GMLParserUtil.java
+++
@@ -1,6 +0,0 @@
-package org.serenityos.gml.plugin.parser;
-
-import com.intellij.lang.parser.GeneratedParserUtilBase;
-
-public class GMLParserUtil extends GeneratedParserUtilBase {
-}
--- src/main/resources/_GMLLexer.flex
+++ src/main/resources/_GMLLexer.flex
@@ -1,4 +1,4 @@
-package ;
+package org.serenityos.gml.plugin.lexer;
 
 import com.intellij.lexer.FlexLexer;
 import com.intellij.psi.tree.IElementType;
